fa866dd0114078066c87ac0595b6e43da23ec7b2
TangHanF <guofu_gh@163.com>
2018-09-20 14:00:21 +0800

    调整字体存放路径

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,7 +75,8 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('css/[name].[hash:7].[ext]')
         }
       }
     ]
